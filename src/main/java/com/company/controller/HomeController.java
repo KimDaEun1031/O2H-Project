5acab7140bf6db7c90174ef0a228bc7b85df0fa6
seo youngho <youngho0983@naver.com>
2021-01-29 23:53:23 +0900

    개인 css 복잡해서 상의후에 정리 해야 할 듯

--- src/main/java/com/company/controller/HomeController.java
+++ src/main/java/com/company/controller/HomeController.java
@@ -11,22 +11,29 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-import lombok.extern.slf4j.Slf4j;
-
-@Slf4j
+/**
+ * Handles requests for the application home page.
+ */
 @Controller
 public class HomeController {
 	
+	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
 	
-	
-	
-	@RequestMapping("/")
-	public String main() {
-		log.info("메인 게시판 요청");
+	/**
+	 * Simply selects the home view to render by returning its name.
+	 */
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String home(Locale locale, Model model) {
+		logger.info("Welcome home! The client locale is {}.", locale);
 		
+		Date date = new Date();
+		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
 		
+		String formattedDate = dateFormat.format(date);
 		
-		return "HYviews/mainPage";
+		model.addAttribute("serverTime", formattedDate );
+		
+		return "home";
 	}
 	
 }
