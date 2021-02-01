3ab070070111e2a5034b8587e6c82a26d348adde
KimDaEun1031 <kimde1031@gmail.com>
2021-02-01 14:41:04 +0900

    commit

--- src/main/java/com/company/controller/HomeController.java
+++ src/main/java/com/company/controller/HomeController.java
@@ -11,7 +11,6 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-import jdk.internal.org.jline.utils.Log;
 
 /**
  * Handles requests for the application home page.
