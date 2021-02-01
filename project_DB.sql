6e0066934391caf60c28b189884de7f37b8457ff
KimDaEun1031 <kimde1031@gmail.com>
2021-02-01 16:16:10 +0900

    DB 생성

---
+++ project_DB.sql
@@ -0,0 +1,15 @@
+--회원가입 테이블
+create table register (
+    userName varchar2(10) primary key,
+	userId varchar2(20) not null,
+    password varchar2(30) not null,
+    address varchar2 (100) not null,
+    phoneNumber varchar2(13) not null,
+    userEmail varchar2(50) not null,
+    userBirth varchar2(12) not null,
+    interest1 varchar2(50) not null,
+    interest2 varchar2(50) not null,
+    interest3 varchar2(50) not null,
+    regDate date default sysdate,
+    auth char(1) default 0
+    );
