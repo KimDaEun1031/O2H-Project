6b639f75a999f180438700bda4e3e93c5f2a8ae3
jingu <bluesheep123@naver.com>
2021-02-04 15:03:45 +0900

    QandA게시판추가

--- src/main/java/com/company/controller/YHQandABoardController.java
+++ src/main/java/com/company/controller/YHQandABoardController.java
@@ -1,11 +1,16 @@
 package com.company.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.company.domain.YHCriteria;
+import com.company.domain.YHPageVO;
 import com.company.domain.YHQandABoardVO;
 import com.company.service.YHQandABoardService;
 
@@ -19,11 +24,28 @@ public class YHQandABoardController {
 	YHQandABoardService service;
 	
 	@GetMapping("/gotoQandAList")
-	public String goToQandABoard() {
-		log.info("큐엔에이 게시판으로 이동");
+	public String goToQandABoardList(Model model,YHCriteria cri) {
+		log.info("Q&A 게시판으로 이동"+cri.getPageNum());
+		List<YHQandABoardVO> list=service.QandABoardList(cri);
+		int total=service.getTotalCnt(cri);
+		model.addAttribute("list",list);
+	
 		
+		
+		model.addAttribute("pageVO",new YHPageVO(cri,total));
 		return "/board/board_QandA_list";
 	}
+	
+	@GetMapping("/read")
+	public String get(int bno,Model model) {
+		log.info("Q&A 단일 게시물 이동");
+		YHQandABoardVO vo=service.QandASelect(bno);
+		model.addAttribute("vo", vo);
+		
+		return "/board/board_QandA_read";
+		
+	}
+	
 	
 	
 	@GetMapping("/register")
@@ -49,5 +71,8 @@ public class YHQandABoardController {
 		}
 		return "/board/board_QandA_write";
 	}
+	
+
+	
 	
 }
--- src/main/java/com/company/mapper/YHQandABoardMapper.java
+++ src/main/java/com/company/mapper/YHQandABoardMapper.java
@@ -1,9 +1,19 @@
 package com.company.mapper;
 
 
+import java.util.List;
+
+import com.company.domain.YHCriteria;
 import com.company.domain.YHQandABoardVO;
 
 public interface YHQandABoardMapper {
 
 	public int insert(YHQandABoardVO board);
+	
+	public List<YHQandABoardVO> QandASearchList(YHCriteria cri);
+
+	public YHQandABoardVO QandASelect(int bno) ;
+	
+	public int totalCnt(YHCriteria cri);
+
 }
