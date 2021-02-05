eb0800dcc14a558b39f5c32e10987451a77cb690
hyunyoung <lhy76927@gmail.com>
2021-02-05 16:46:56 +0900

    profile_setting + 파일 업로드 servlet-context.xml 수정

---
+++ src/main/java/com/company/domain/HYChangeVO.java
@@ -0,0 +1,26 @@
+package com.company.domain;
+
+import java.util.List;
+
+import lombok.Data;
+
+@Data
+public class HYChangeVO {
+	//아이디-비번은 세션에서 받아오고
+	private String userId;
+	private String password;
+	//여기는 SETTING 페이지에서 수정?
+	private String userName;
+	private String userEmail;
+	private String new_password;
+	private String confirm_password;
+	private String userBirth;
+	
+	//파일 첨부 목록
+	private List<HYFileAttach> attachList;
+	
+	
+	public boolean newPasswordEqualsConfirmPassword() {
+		return new_password.equals(confirm_password);
+	}
+}
--- src/main/java/com/company/mapper/HYMapper.java
+++ src/main/java/com/company/mapper/HYMapper.java
@@ -1,9 +1,23 @@
 package com.company.mapper;
 
+import java.util.List;
+
+import com.company.domain.HYChangeVO;
+import com.company.domain.HYFileAttach;
 import com.company.domain.HYLoginVO;
 import com.company.domain.KDLoginVO;
 import com.company.domain.LoginVO;
 
 public interface HYMapper {
+	//탈퇴
 	public int leaveMember(HYLoginVO hylogin);
+	//profile 부분-보드mapper
+	public int updateMember(HYChangeVO change);
+	public List<HYFileAttach> attachList(String userId);
+	//profile 첨부파일-attachMapper
+	public int insertAttach(HYFileAttach attach);
+	public int deleteAttach(String userId);
+	
+	
+	
 }
